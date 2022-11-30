1ccf3c31e791e552da6d81f963577f91dbf83479
parsonsmatt <parsonsmatt@gmail.com>
2022-11-30 13:27:31 -0700

    Fix the result type in momorphic update
    This commit usees the constraint trick to fix the output type for
    monomorphic record set. Now GHC can infer the type of a monomorphic
    update without problem.

--- recupd.hs
+++ recupd.hs
@@ -1,6 +1,8 @@
 {-# language DuplicateRecordFields #-}
+{-# language OverloadedLabels #-}
 {-# language DataKinds #-}
 
+import GHC.OverloadedLabels
 import GHC.TypeLits
 import Data.Proxy
 
@@ -9,18 +11,23 @@ data A = A { name :: String, age :: Int }
 data B = B { name :: Int, age :: String}
 
 class UpdateField (sym :: Symbol) s t a where
-    updateField :: proxy sym -> a -> s -> t
+    updateField :: Proxy sym -> a -> s -> t
 
-instance UpdateField "name" A A String where
+-- In the case of monomorphic update, we can introduce a constraint that
+-- should fix the target type.
+instance (A ~ t) => UpdateField "name" A t String where
     updateField _ str (A _oldStr oldAge) = A str oldAge
 
-instance UpdateField "age" A A Int where
+instance (A ~ t) => UpdateField "age" A t Int where
     updateField _ age (A oldStr _oldAge) = A oldStr age
 
-instance UpdateField "name" B B Int where
+instance (B ~ t) => UpdateField "name" B t Int where
     updateField _ int (B _oldInt oldStr) = B int oldStr
 
-instance UpdateField "age" B B String where
+instance (B ~ t) => UpdateField "age" B t String where
     updateField _ str (B oldInt _oldStr) = B oldInt str
 
-foo = updateField (Proxy @"name") "hello" (A { name = "goodbye", age = 10 })
+instance (KnownSymbol sym, sym ~ sym') => IsLabel sym (Proxy sym') where
+    fromLabel = Proxy
+
+singleUpdate = updateField #name "hello" (A { name = "goodbye", age = 10 })
