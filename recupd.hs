b79b702656a46712d83a12e3f83893530c622e4a
parsonsmatt <parsonsmatt@gmail.com>
2022-11-30 13:24:00 -0700

    This commit shows a first stab at the problem.
    Unfortunately, we have a type error. This is ambiguous. We don't know
    what the result type should be, because there's no functional dependency
    on the class.

--- recupd.hs
+++ recupd.hs
@@ -1,7 +1,26 @@
 {-# language DuplicateRecordFields #-}
+{-# language DataKinds #-}
 
-data A = A { name :: String }
+import GHC.TypeLits
+import Data.Proxy
 
-data B = B { name :: Int }
+data A = A { name :: String, age :: Int }
 
-class UpdateField
+data B = B { name :: Int, age :: String}
+
+class UpdateField (sym :: Symbol) s t a where
+    updateField :: proxy sym -> a -> s -> t
+
+instance UpdateField "name" A A String where
+    updateField _ str (A _oldStr oldAge) = A str oldAge
+
+instance UpdateField "age" A A Int where
+    updateField _ age (A oldStr _oldAge) = A oldStr age
+
+instance UpdateField "name" B B Int where
+    updateField _ int (B _oldInt oldStr) = B int oldStr
+
+instance UpdateField "age" B B String where
+    updateField _ str (B oldInt _oldStr) = B oldInt str
+
+foo = updateField (Proxy @"name") "hello" (A { name = "goodbye", age = 10 })
